43b377b1869cbb17dae20aa7cc223b9d1ffef921
limengxu <1354786446@qq.com>
2023-02-24 02:08:51 +0800

    添加四个路由

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,59 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { ErrorPage } from './components/ErrorPage'
+import { NavLink, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RedirectToWelcome1 } from './components/RedirectToWelcome1'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>root</div>,
-    errorElement: <ErrorPage/>
-  },
-  {
-    path: '/1',
-    element: <div>1</div>
+    element: <Outlet/>,
+    errorElement: <RedirectToWelcome1 />,
+    children: [
+      { index: true, element: <div>空</div> },
+      {
+        path: 'welcome',
+        element: <Outlet />,
+        children: [
+          { index: true, element: <div>lmxu</div> },
+          {
+            path: '1',
+            element: (
+              <div>
+                1
+                <NavLink to='/welcome/2'>下一页</NavLink>
+              </div>
+            )
+          },
+          {
+            path: '2',
+            element: (
+              <div>
+                2
+                <NavLink to='/welcome/3'>下一页</NavLink>
+              </div>
+            )
+          },
+          {
+            path: '3',
+            element: (
+              <div>
+                3
+                <NavLink to='/welcome/4'>下一页</NavLink>
+              </div>
+            )
+          },
+          {
+            path: '4',
+            element: (
+              <div>
+                4
+                <NavLink to='xxxx'>下一页</NavLink>
+              </div>
+            )
+          },
+        ]
+      }
+    ]
   }
 ])
 
