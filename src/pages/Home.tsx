dd687b2998b4398e2ed0b543ad801fca8734a4b7
limengxu <1354786446@qq.com>
2023-03-01 01:36:32 +0800

    演示如何设置页面标题 title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,13 @@ import { Navigate } from 'react-router-dom'
 import p from '../assets/images/pig.svg'
 import add from '../assets/icons/add.svg'
 import { ajax } from '../lib/ajax'
+import { useTitle } from '../hooks/useTitle'
 
-export const Home: React.FC = () => {
+interface Props {
+  title: string
+}
+export const Home: React.FC<Props> = (props) => {
+  useTitle(props.title)
   const { data: meData, error: meError } = useSWR('/api/v1/me', async path =>
     (await ajax.get<Resource<User>>(path)).data.resource
   )
