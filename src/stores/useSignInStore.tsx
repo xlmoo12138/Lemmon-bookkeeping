bed7f8dbca2757e9825a235c338c24196bf9cd72
limengxu <1354786446@qq.com>
2023-03-12 01:24:08 +0800

    表单提交事件可以获取到表单数据

---
+++ src/stores/useSignInStore.tsx
@@ -0,0 +1,28 @@
+import { create } from 'zustand'
+
+type Data = {
+  email: string
+  code: string
+}
+interface SignIn {
+  data: Data
+  setData: (data: Partial<Data>) => void
+}
+export const useSignInStore = create<SignIn>((set, get) => (
+  {
+    data: {
+      email: '',
+      code: ''
+    },
+    setData: (data: Partial<Data>) => {
+      set(state => (
+        {
+          data: {
+            ...state.data,
+            ...data
+          }
+        }
+      ))
+    }
+  }
+))
