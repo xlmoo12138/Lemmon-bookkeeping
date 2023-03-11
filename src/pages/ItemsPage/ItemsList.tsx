d548039b85e2fe792625106c4dcd93030b8eeb2b
limengxu <1354786446@qq.com>
2023-03-11 16:11:25 +0800

    可以请求所有页数的数据了

--- src/pages/ItemsPage/ItemsList.tsx
+++ src/pages/ItemsPage/ItemsList.tsx
@@ -3,39 +3,52 @@ import { ajax } from '../../lib/ajax'
 interface Props {
 }
 
-const getKey = (pageIndex: number) => {
+const getKey = (pageIndex: number, prev: Resources<Item>) => {
+  if (prev) {
+    const sendCount = (prev.pager.page - 1) * prev.pager.per_page + prev.resources.length
+    const count = prev.pager.count
+    if (sendCount >= count) {
+      return null
+    }
+  }
   return `/api/v1/items?page=${pageIndex + 1}`
 }
 export const ItemsList: React.FC<Props> = () => {
-  const { data, error } = useSWRInfinite(
+  const { data, error, size, setSize } = useSWRInfinite(
     getKey, async path => (await ajax.get<Resources<Item>>(path)).data
   )
-  window.console.log(data, error)
-  const items: Item[] = []
-  return (
-    <div>
-      <ol>
-        {items.map(item =>
-          <li key={item.id} grid grid-cols="[auto_1fr_auto]" grid-rows-2 px-16px py-8px gap-x-12px
-            border-b-1 b="#eee">
-            <div row-start-1 col-start-1 row-end-3 col-end-2 text-24px w-48px h-48px
-              bg="#D8D8D8" rounded="50%" flex justify-center items-center >
-              😘
-            </div>
-            <div row-start-1 col-start-2 row-end-2 col-end-3 >
-              旅行
-            </div>
-            <div row-start-2 col-start-2 row-end-3 col-end-4 text="#999999" >
-              2023 年 3 月 2 日
-            </div>
-            <div row-start-1 col-start-3 row-end-2 col-end-4 text="#53A867" >
-              ¥ 520
-            </div>
-          </li>)}
-      </ol>
+  console.log(data)
+  const onLoadMore = () => {
+    setSize(size + 1)
+  }
+  if (!data) {
+    return <div>还没搞定</div>
+  } else {
+    return <>
+      <ol>{
+      data.map(({ resources }) => {
+        return resources.map(item =>
+      <li key={item.id} grid grid-cols="[auto_1fr_auto]" grid-rows-2 px-16px py-8px gap-x-12px
+        border-b-1 b="#eee">
+        <div row-start-1 col-start-1 row-end-3 col-end-2 text-24px w-48px h-48px
+          bg="#D8D8D8" rounded="50%" flex justify-center items-center >
+          😘
+        </div>
+        <div row-start-1 col-start-2 row-end-2 col-end-3 >
+          旅行
+        </div>
+        <div row-start-2 col-start-2 row-end-3 col-end-4 text="#999999" >
+          2023 年 3 月 2 日
+        </div>
+        <div row-start-1 col-start-3 row-end-2 col-end-4 text="#53A867" >
+          ¥ {item.amount / 100}
+        </div>
+      </li>)
+      })
+      }</ol>
       <div p-16px>
-        <button j-btn>加载更多</button>
+        <button j-btn onClick={onLoadMore}>加载更多</button>
       </div>
-    </div>
-  )
+    </>
+  }
 }
