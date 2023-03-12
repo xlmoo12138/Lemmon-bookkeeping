a4c879db04cfbf81fc5710dd53aed801b3792dc7
limengxu <1354786446@qq.com>
2023-03-12 16:36:07 +0800

    修改 Welcome 界面，即使页面被浏览器挡住，也不会影响交互

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -37,7 +37,7 @@ export const WelcomeLayout: React.FC = () => {
   })
 
   const main = useRef<HTMLElement>(null)
-  const { direction } = useSwipe(main, { onTouchStart: e => e.preventDefault() })
+  const { direction } = useSwipe(main)
   const nav = useNavigate()
   useEffect(() => {
     if (direction === 'left') {
@@ -52,9 +52,8 @@ export const WelcomeLayout: React.FC = () => {
     setHasReadWelcomes(true)
   }
   return (
-    <div className="bg-#5f34bf" h-screen flex flex-col
-      items-stretch pb-16px
-    >
+    <div className="bg-#5f34bf" h-screen flex flex-col items-stretch pb-16px >
+      <Link fixed text-white top-16px right-16px text-32px to="/welcome/xxx" onClick={onSkip} >跳过</Link>
       <header shrink-0 text-center>
         <img src={logo} w-64px h-69px />
         <h1 text="#D4D4EE">柠萌记账</h1>
@@ -68,10 +67,6 @@ export const WelcomeLayout: React.FC = () => {
           </animated.div>
         )}
       </main>
-      <footer shrink-0 text-center text-24px text-white grid grid-cols-3 grid-rows-1>
-        <Link style={{ gridArea: '1 / 2 / 2 / 3' }} to={linkMap[location.pathname]}>下一页</Link>
-        <Link style={{ gridArea: '1 / 3 / 2 / 4' }} to='/home' onClick={onSkip}>跳过</Link>
-      </footer>
     </div>
   )
 }
