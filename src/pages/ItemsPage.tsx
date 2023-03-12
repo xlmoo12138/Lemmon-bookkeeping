82f810c8ad1e38fbfc7fd1395bffbae4dae01af4
limengxu <1354786446@qq.com>
2023-03-12 22:47:22 +0800

    抽离出 Tabs.tsx 泛型组件

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -21,7 +21,7 @@ export const ItemsPage: React.FC = () => {
           <Icon name="menu" className="w-24px h-24px"
             onClick={() => { setVisible(!visible) }} />
         } />
-        <TimeRangePicker selected={timeRange} onSelected={setTimeRange} />
+        <TimeRangePicker selected={timeRange} onSelect={setTimeRange} />
       </Gradient>
       <ItemsSummary />
       <ItemsList />
