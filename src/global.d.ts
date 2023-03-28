5b2c3470004c3fbea0b9cb1372b4966848ba05c3
limengxu <1354786446@qq.com>
2023-03-28 21:03:58 +0800

    验证新建标签表单的正确性

--- src/global.d.ts
+++ src/global.d.ts
@@ -34,3 +34,13 @@ interface Item {
   deleted_at?: string
 }
 
+interface Tag {
+  id: number
+  kind: Item['kind']
+  user_id: number
+  name: string
+  sign: string
+  deleted_at: string
+  created_at: string
+  updated_at: string
+}
