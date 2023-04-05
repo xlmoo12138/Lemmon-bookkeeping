8721b60eb3295df6936290df4d510def3f057778
limengxu <1354786446@qq.com>
2023-04-06 02:57:04 +0800

    /items/new 页面必须登录之后才能看到 所以需要在路由中添加一个 loader，用来预加载数据，如果数据加载失败，就会跳转到登录页面。

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -17,6 +17,7 @@ import { Welcome3 } from '../pages/Welcome3'
 import { Welcome4 } from '../pages/Welcome4'
 import { ItemsPageError } from '../pages/ItemsPageError'
 import { ErrorEmptyData, ErrorUnauthorized } from '../errors'
+import { ErrorPage } from '../pages/ErrorPage'
 
 export const router = createBrowserRouter([
   { path: '/', element: <Root />, },
@@ -50,7 +51,21 @@ export const router = createBrowserRouter([
       })
     }
   },
-  { path: '/items/new', element: <ItemsNewPage /> },
+  {
+    path: '/items/new',
+    element: <ItemsNewPage />,
+    errorElement: <ErrorPage/>,
+    loader: async () => {
+      const onError = (error: AxiosError) => {
+        if (error.response?.status === 401) { throw new ErrorUnauthorized() }
+        throw error
+      }
+      return preload('/api/v1/me', async (path) => {
+        const response = await axios.get<Resource<User>>(path).catch(onError)
+        return response.data
+      })
+    }
+  },
   { path: '/tags', element: <div>标签</div> },
   { path: '/tags/new', element: <TagsNewPage/> },
   { path: '/tags/:id', element: <TagsEditPage /> },
