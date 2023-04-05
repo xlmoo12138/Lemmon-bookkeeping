d9c7dcfd15d5e14bff373962b0da303c315fc860
limengxu <1354786446@qq.com>
2023-04-06 03:25:27 +0800

    把所有需要登录的页面都放到一个路由组里

--- mock/me.mock.ts
+++ mock/me.mock.ts
@@ -3,16 +3,16 @@ import type { MockMethod } from 'vite-plugin-mock'
 export const meMock: MockMethod = {
   url: '/api/v1/me',
   method: 'get',
-  statusCode: 401,
+  statusCode: 200,
   response: () => {
-    return ''
-    // {
-    //   resource: {
-    //     id: 1,
-    //     email: 'kirdoyz@gmail.com',
-    //     updated_at: '2021-08-01T00:00:00.000Z',
-    //     created_at: '2021-08-01T00:00:00.000Z',
-    //   }
-    // }
+    // return ''
+    return {
+      resource: {
+        id: 1,
+        email: 'kirdoyz@gmail.com',
+        updated_at: '2021-08-01T00:00:00.000Z',
+        created_at: '2021-08-01T00:00:00.000Z',
+      }
+    }
   }
 }
