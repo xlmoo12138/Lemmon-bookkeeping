bb34b19522f771cb18235181b5f492e2c553e17b
limengxu <1354786446@qq.com>
2023-04-06 14:00:20 +0800

    收集新建 item 表单的 kind 字段

---
+++ src/stores/useCreateItemStore.tsx
@@ -0,0 +1,48 @@
+import { create } from 'zustand'
+import type { FormError } from '../lib/validate'
+
+type Data = Item
+
+type CreateItem = {
+  data: Partial<Data>
+  error: FormError<Data>
+  setData: (data: Partial<Data>) => void
+  setError: (error: Partial<FormError<Data>>) => void
+}
+export const useCreateItemStore = create<CreateItem>((set, get) => {
+  return {
+    data: {
+      kind: 'expenses',
+      tag_ids: [],
+      happen_at: '',
+      amount: 0
+    },
+    error: {
+      data: [],
+      tag_ids: [],
+      happen_at: [],
+      amount: []
+    },
+    setData: (data: Partial<Data>) => {
+      set(state => (
+        {
+          ...state,
+          data: {
+            ...state.data,
+            ...data
+          }
+        }
+      ))
+    },
+    setError: (error: Partial<FormError<Data>>) => {
+      set(state => (
+        {
+          ...state,
+          error: {
+            ...error
+          }
+        }
+      ))
+    }
+  }
+})
