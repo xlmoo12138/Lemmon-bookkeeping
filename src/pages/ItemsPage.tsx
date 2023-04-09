a895cca3e9961d6d50976577e83613eb4e512892
limengxu <1354786446@qq.com>
2023-04-09 23:52:03 +0800

    ItemsPage 在用户切换时间时会重新请求

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -7,12 +7,14 @@ import { TimeRangePicker } from '../components/TimeRangePicker'
 import { TopMenu } from '../components/TopMenu'
 import { TopNav } from '../components/TopNav'
 import { useMenuStore } from '../stores/useMenuStore'
+import { timeRangeToStartAndEnd } from '../lib/timeRangeToStartAndEnd'
 import { ItemsList } from './ItemsPage/ItemsList'
 import { ItemsSummary } from './ItemsPage/ItemsSummary'
 
 export const ItemsPage: React.FC = () => {
   const [timeRange, setTimeRange] = useState<TimeRange>('thisMonth')
   const { visible, setVisible } = useMenuStore()
+  const { start, end } = timeRangeToStartAndEnd(timeRange)
 
   return (
     <div>
@@ -24,7 +26,7 @@ export const ItemsPage: React.FC = () => {
       </Gradient>
         <TimeRangePicker selected={timeRange} onSelect={setTimeRange} />
       <ItemsSummary />
-      <ItemsList />
+      <ItemsList start={start} end={end} />
       <AddItemFloatButton />
       <TopMenu visible={visible} onClickMask={() => { setVisible(false) }} />
     </div>
