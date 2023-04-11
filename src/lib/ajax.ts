1df65e3291c8d4358786e3cf2f7f61e16da4852d
limengxu <1354786446@qq.com>
2023-04-12 01:54:04 +0800

    封装 axios 为 ajax，提供了默认配置

--- src/lib/ajax.ts
+++ src/lib/ajax.ts
@@ -3,23 +3,19 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { useLoadingStore } from '../stores/useLoadingStore'
 
-let hasSetup = false
-export const setup = () => {
-  if (hasSetup) { return }
-  hasSetup = true
-  // 静态配置项直接用 defaults 配置
-  axios.defaults.baseURL = isDev ? '/' : 'http://121.196.236.94:8080/'
-  axios.defaults.headers.post['Content-Type'] = 'application/json'
-  axios.defaults.timeout = 10000
-
-  // 动态配置项用拦截器配置
-  axios.interceptors.request.use((config) => {
-    const jwt = localStorage.getItem('jwt') || ''
-    config.headers = config.headers || {}
-    if (jwt) { config.headers.Authorization = `Bearer ${jwt}` }
-    return config
-  })
-}
+export const ajax = axios.create({
+  baseURL: isDev ? '/' : 'http://121.196.236.94:8080/',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  timeout: 10000
+})
+ajax.interceptors.request.use((config) => {
+  const jwt = localStorage.getItem('jwt') || ''
+  config.headers = config.headers || {}
+  if (jwt) { config.headers.Authorization = `Bearer ${jwt}` }
+  return config
+})
 
 type Options = {
   showLoading?: boolean
@@ -49,28 +45,27 @@ export const useAjax = (options?: Options) => {
     }
     throw error
   }
-  const ajax = {
+  return {
     get: <T>(path: string, config?: AxiosRequestConfig<any>) => {
-      return axios.get<T>(path, config).catch(onError)
+      return ajax.get<T>(path, config).catch(onError)
     },
     post: <T>(path: string, data: JSONValue) => {
       if (showLoading) { setVisible(true) }
-      return axios.post<T>(path, data).catch(onError).finally(() => {
+      return ajax.post<T>(path, data).catch(onError).finally(() => {
         if (showLoading) { setVisible(false) }
       })
     },
     patch: <T>(path: string, data: JSONValue) => {
       if (showLoading) { setVisible(true) }
-      return axios.patch<T>(path, data).catch(onError).finally(() => {
+      return ajax.patch<T>(path, data).catch(onError).finally(() => {
         if (showLoading) { setVisible(false) }
       })
     },
     destroy: <T>(path: string) => {
       if (showLoading) { setVisible(true) }
-      return axios.delete<T>(path).catch(onError).finally(() => {
+      return ajax.delete<T>(path).catch(onError).finally(() => {
         if (showLoading) { setVisible(false) }
       })
     },
   }
-  return ajax
 }
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,6 +1,5 @@
 import { Outlet, createBrowserRouter } from 'react-router-dom'
 import type { AxiosError } from 'axios'
-import axios from 'axios'
 import { Root } from '../components/Root'
 import { WelcomeLayout } from '../layouts/WelcomeLayout'
 import { Home } from '../pages/Home'
@@ -17,6 +16,7 @@ import { Welcome4 } from '../pages/Welcome4'
 import { ItemsPageError } from '../pages/ItemsPageError'
 import { ErrorEmptyData, ErrorUnauthorized } from '../errors'
 import { ErrorPage } from '../pages/ErrorPage'
+import { ajax } from '../lib/ajax'
 
 export const router = createBrowserRouter([
   { path: '/', element: <Root />, },
@@ -38,8 +38,9 @@ export const router = createBrowserRouter([
     element: <Outlet />,
     errorElement: <ErrorPage/>,
     loader: async () => {
-      return await axios.get<Resource<User>>('/api/v1/me').catch(e => {
-        if (e.response?.status === 401) { throw new ErrorUnauthorized() }
+      return await ajax.get<Resource<User>>('/api/v1/me').catch(error => {
+        if (error.response?.status === 401) { throw new ErrorUnauthorized() }
+        throw error
       })
     },
     children: [
@@ -52,7 +53,7 @@ export const router = createBrowserRouter([
             if (error.response?.status === 401) { throw new ErrorUnauthorized() }
             throw error
           }
-          const response = await axios.get<Resources<Item>>('/api/v1/items?page=1').catch(onError)
+          const response = await ajax.get<Resources<Item>>('/api/v1/items?page=1').catch(onError)
           if (response.data.resources.length > 0) {
             return response.data
           } else {
