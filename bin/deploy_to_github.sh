cb24a3899294d4f2463a25b5552c98e22f54f40a
limengxu <1354786446@qq.com>
2023-04-13 18:46:49 +0800

    使用 deploy_to_github.sh 部署网站到 github.com

--- bin/deploy_to_github.sh
+++ bin/deploy_to_github.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/env bash
+function set_env {
+  name=$1
+  hint=$2
+  [[ ! -z "${!name}" ]] && return
+  while [ -z "${!name}" ]; do
+    [[ ! -z "$hint" ]] && echo "> 请输入 $name: $hint" || echo "> 请输入 $name:"
+    read $name
+  done
+  if [ ! -s ./.env ]; then
+    echo "export $name=\"${!name}\"" > ./.env
+  else
+    sed -i "1s/^/export $name=\"${!name}\"\n/" ./.env
+  fi
+
+  echo "${name} 已保存至 ./.env 。如果需要修改，请自行编辑 ./.env"
+}
+
+touch ./.env && source ./.env
+set_env user "GitHub 用户名"
+set_env repo "GitHub 仓库名"
+
 rm -rf dist
-npm run build
+npm run build -- --base "$repo"
 cd dist
 git init
 git add .
-git commit -m deploy
-git remote add origin git@github.com:xlmoo12138/react-lemmon-1-preview.git
-git push -f origin master:master
+git commit -m "deploy"
+git remote add origin git@github.com:$user/$repo.git
+git branch -M main
+git push -f origin main:main
+echo "===================================部署成功====================================="
+git open
 cd -
